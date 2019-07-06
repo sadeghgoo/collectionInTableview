5f3880cb2522e007046c7fbefbfb669f4318fe3f
sadeghgoo <sadeghitunes2@gmail.com>
2019-07-06 23:07:20 +0430

    Initial Commit

---
+++ TableviewInCollectionViewTests/TableviewInCollectionViewTests.swift
@@ -0,0 +1,34 @@
+//
+//  TableviewInCollectionViewTests.swift
+//  TableviewInCollectionViewTests
+//
+//  Created by sadegh on 7/6/19.
+//  Copyright © 2019 sadegh. All rights reserved.
+//
+
+import XCTest
+@testable import TableviewInCollectionView
+
+class TableviewInCollectionViewTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
